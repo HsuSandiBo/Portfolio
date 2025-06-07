79cc261d400c40157027242f35c32550c343a40b
HsuSandiBo <susandibo64@gmail.com>
2025-06-07 18:41:08 +0700

    Update My Portfolio

--- photography_project/_classes/Libs/Database/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Libs\Database;
-
-$conn = new mysqli("sql302.infinityfree.com", "if0_34664567_photography", "if0_34664567", "kg8WduyBxqPQZ7z");
-if(!$conn){
-    die(mysqli_error($conn));
-}
